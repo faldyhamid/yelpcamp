d6a7e5e6eb2c855ae20fc2ab2d56f165ea3eeb69
faldyhamid <faldyhamid@gmail.com>
2024-03-13 17:19:40 +0700

    Changed DB Url

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ const userRoutes = require('./routes/users')
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-const dbUrl = 'mongodb://localhost:27017/yelp-camp'; //process.env.DB_URL;
+const dbUrl = process.env.DB_URL; //'mongodb://localhost:27017/yelp-camp'; 
 
 const store = MongoStore.create({
     mongoUrl: dbUrl,
